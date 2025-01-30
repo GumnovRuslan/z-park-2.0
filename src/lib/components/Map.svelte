e8d09af4aea09cedc2e16d4c1feef8c178db026a
GumnovRuslan <ruslang.crocode@gmail.com>
2025-01-30 15:00:11 +0300

    added point for each size

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import Point from "./Point.svelte";
 
+  interface IProps {
+    isOpenModal: boolean
+  }
+
   const points = [
     {
       path_imge: '/img/place/бар1.png',
@@ -34,18 +38,100 @@
       path_imge: '/img/place/кафе1.png',
       class_name: 'caffe2'
     },
+    {
+      path_imge: '/img/place/комната для др1.png',
+      class_name: 'birthday-room1'
+    },
+    {
+      path_imge: '/img/place/комната для др2.png',
+      class_name: 'birthday-room2'
+    },
+    {
+      path_imge: '/img/place/комната для др2.png',
+      class_name: 'birthday-room2-2'
+    },
+    {
+      path_imge: '/img/place/кухня1.png',
+      class_name: 'kitchen'
+    },
+    {
+      path_imge: '/img/place/лабиринт1.png',
+      class_name: 'maze'
+    },
+    {
+      path_imge: '/img/place/лого1.png',
+      class_name: 'logo'
+    },
+    {
+      path_imge: '/img/place/песочница1.png',
+      class_name: 'sandbox'
+    },
+    {
+      path_imge: '/img/place/ресепшн1.png',
+      class_name: 'reception'
+    },
+    {
+      path_imge: '/img/place/тарзанка1.png',
+      class_name: 'bungee'
+    },
+    {
+      path_imge: '/img/place/хоккей1.png',
+      class_name: 'hockey'
+    },
+    {
+      path_imge: '/img/place/яма1.png',
+      class_name: 'pit'
+    },
   ]
 
-  interface IProps {
-    isOpenModal: boolean
+  let { isOpenModal = $bindable() }: IProps = $props()
+  let map: HTMLDivElement
+  let isDragging: boolean = $state(false);
+  let pos = $state({
+    startX: 0,
+    startY: 0,
+    scrollLeft: 0,
+    scrollTop: 0,
+  })
+
+  const startDrag = (e: MouseEvent) => {
+    isDragging = true;
+    pos = {
+      startX: e.pageX - map.offsetLeft,
+      startY: e.pageY - map.offsetTop,
+      scrollLeft: map.scrollLeft,
+      scrollTop: map.scrollTop,
+    }
   }
 
-  let { isOpenModal = $bindable() }: IProps = $props()
+  function stopDrag() {
+    isDragging = false;
+  }
+
+  const drag = (e: MouseEvent, speed: number = 1) => {
+    e.preventDefault();
+    if (!isDragging) return;
+    if (speed <= 0) speed = 0.1;
+    const x = e.pageX - map.offsetLeft;
+    const y = e.pageY - map.offsetTop;
+    const walkX = (x - pos.startX) * speed
+    const walkY = (y - pos.startY) * speed
+    map.scrollLeft = pos.scrollLeft - walkX;
+    map.scrollTop = pos.scrollTop - walkY;
+  }
 </script>
 
-<div class="map">
+<div class="map"
+  role="presentation"
+  bind:this={map}
+  onmousedown={startDrag}
+  onmouseup={stopDrag}
+  onmouseleave={stopDrag}
+  onmousemove={drag}
+  style:cursor={isDragging ? 'grabbing' : 'grab'}
+>
   <div class="map__img-wrapper">
-    <img class="map__img" src="/img/zpark-map.webp" alt="map">
+    <img class="map__img" src="/img/zpark-map.webp" alt="map" draggable="false">
     {#each points as pointData}
       <Point bind:isOpenModal={isOpenModal} data={pointData}/>
     {/each}
@@ -54,6 +140,7 @@
 
 <style lang="scss">
   @use "/static/styles/mixins" as mixins;
+  
   .map {
     width: 100vw;
     height: 100vh;
@@ -66,23 +153,32 @@
 
     &__img-wrapper {
       position: relative;
-
       width: max-content;
       height: max-content;
-      // max-width: 2500px;
     }
 
     &__img {
+      position: relative;
+      z-index: -1;
       display: block;
-      max-width: 2500px;
-      // width: 110%;
+      width: 2500px;
+      user-select: none;
+      pointer-events: none;
 
-      @include mixins.media-breakpoint-up(xxl) {
-        // max-width: 120%;
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 4000px;
       }
 
-      @include mixins.media-breakpoint-down(xl) {
-        // object-fit: cover;
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 3000px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 2500px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 3000px;
       }
     }
   }
--- src/lib/components/Point.svelte
+++ src/lib/components/Point.svelte
@@ -12,18 +12,18 @@
 </script>
 
 <button class="point point__btn point--{data.class_name}" type="button" onclick={() => isOpenModal = true}>
-  <img src={data.path_imge} alt="alt" class="point__img" >
+  <img src={data.path_imge} alt="alt" class="point__img" draggable="false">
 </button>
 
 <style lang="scss">
+  @use "/static/styles/mixins" as mixins;
+
   .point {
     &__btn {
       position: absolute;
       z-index: 1;
-      // width: 500px;
       cursor: pointer;
-      border: none;
-      transition: 0.3s ease-in-out;
+      transition: transform 0.2s ease-in-out;
 
       &:hover {
         transform: scale(110%);
@@ -31,57 +31,414 @@
     }
 
     &--bar {
-      top: 19.5%;
-      left: 55%;
-      width: 180px;
+      top: 19.4%;
+      left: 54.7%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 306px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 232px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 190px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 232px;
+      }
     }
 
     &--trampolines {
       top: 79%;
       left: 31%;
-      width: 234px;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 376px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 281px;
+
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 234px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 281px;
+      }
     }
 
     &--tower {
       top: 47.7%;
       left: 55.5%;
-      width: 235px;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 376px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 283px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 235px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 283px;
+      }
     }
 
     &--slide {
       top: 78.5%;
       left: 63.3%;
-      width: 235px;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 376px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 283px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 235px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 283px;
+      }
     }
 
     &--disco {
       top: 64.4%;
       left: 77%;
-      width: 235px;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 376px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 283px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 235px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 283px;
+      }
     }
 
     &--playground {
       top: 25.5%;
       left: 38.8%;
-      width: 218px;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 348px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 261px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 218px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 261px;
+      }
     }
 
     &--caffe1 {
       top: 33.2%;
       left: 35.4%;
-      width: 218px;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 348px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 261px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 218px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 261px;
+      }
     }
 
     &--caffe2 {
       top: 37.6%;
       left: 49.5%;
-      width: 218px;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 348px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 261px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 218px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 261px;
+      }
+    }
+
+    &--birthday-room1 {
+      top: 27.3%;
+      left: 68.4%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 381px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 285px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 240px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 285px;
+      }
+    }
+
+    &--birthday-room2 {
+      top: 41.2%;
+      left: 73%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 381px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 285px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 240px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 285px;
+      }
+    }
+
+    &--birthday-room2-2 {
+      top: 76.4%;
+      left: 39%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 381px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 287px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 240px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 287px;
+      }
+    }
+
+    &--kitchen {
+      top: 21.3%;
+      left: 45.9%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 348px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 261px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 218px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 261px;
+      }
+    }
+
+    &--maze {
+      top: 52%;
+      left: 63.9%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 376px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 281px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 235px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 281px;
+      }
+    }
+
+    &--logo {
+      top: 3.4%;
+      left: 36.2%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 1145px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 858px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 714px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 858px;
+      }
+
+      &:hover {
+        transform: scale(103%);
+      }
+    }
+
+    &--sandbox {
+      top: 34.3%;
+      left: 19.7%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 455px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 341px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 286px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 341px;
+      }
+    }
+
+    &--reception {
+      top: 57.3%;
+      left: 42.4%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 376px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 283px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 235px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 283px;
+      }
+    }
+
+    &--bungee {
+      top: 60%;
+      left: 26.4%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 496px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 372px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 310px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 372px;
+      }
+    }
+
+    &--hockey {
+      top: 19.1%;
+      left: 62.7%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 258px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 194px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 161px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 194px;
+      }
+    }
+
+    &--pit {
+      top: 79.6%;
+      left: 19.2%;
+
+      @include mixins.media-breakpoint-up(xxxl) {
+        width: 376px;
+      }
+
+      @include mixins.media-breakpoint-between(xl, xxxl) {
+        width: 283px;
+      }
+
+      @include mixins.media-breakpoint-between(md, xl) {
+        width: 235px;
+      }
+
+      @include mixins.media-breakpoint-down(md) {
+        width: 283px;
+      }
     }
 
     &__img {
       width: 100%;
       height: 100%;
       object-fit: contain;
+      user-select: none;
     }
   }
 </style>
